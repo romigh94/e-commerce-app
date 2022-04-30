3028a2f496fd3d3e342cbcdc91c51d1ccc04ae22
Romina Ghaderi <romina.ghaderi@outlook.com>
2022-05-01 01:34:19 +0200

    displaying products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import data from './Productdata'
 
 const Products = () => {
   return (
-    <div>Products</div>
+<div>
+    <h2>Exempel på produkter</h2>
+        <div id="product-container">
+            {data.Items.map((product) => {
+            return (
+                <div key={product.id} item={product} className="products">
+                    <img
+                        src={product.image}
+                        alt={product.name}
+                        width="100"
+                        height="100"
+                        className="item-img"
+                        ></img>
+                        <h5>{product.name}</h5>
+                        <p className="item-price">{product.price} kr</p>
+                    <button className="to-cart-btn">
+                        Add to cart
+                    </button>
+                </div>
+          )
+        })}
+    </div>
+</div>
   )
 }
 
