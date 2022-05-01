5b0afd9945615eb13e203a1f1d1c71fcdc3bdbb4
Romina Ghaderi <romina.ghaderi@outlook.com>
2022-05-01 19:01:56 +0200

    adding 1 more product and design

--- src/components/Productdata.jsx
+++ src/components/Productdata.jsx
@@ -1,30 +1,36 @@
 import img1 from '../productImages/Product1.jpg'
 import img2 from '../productImages/Product2.jpg'
 import img3 from '../productImages/Product3.jpg'
-
-
+import img4 from '../productImages/Product4.jpg'
 
 
 const data = {
     Items:[
         {
             id: 1,
-            name: "Product 1",
+            name: "Example 1",
             price: 169,
             image: img1
         },
         {
             id: 2,
-            name: "Product 2",
+            name: "Example 2",
             price: 1299,
             image: img2
         },
         {
             id: 3,
-            name: "Product 3",
+            name: "Example 3",
             price: 499,
             image: img3
+        },
+        {
+            id: 4,
+            name: "Example 4",
+            price: 299,
+            image: img4
         }
+
     ]
 }
 
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,27 +6,20 @@ import { useCart } from 'react-use-cart'
 
 const Products = () => {
 
-const {addItem} = useCart();
+const { addItem } = useCart();
 
   return (
 <div>
-    <h2>Examples of products</h2>
+    <h2>Different types of products</h2>
         <div className="product-container">
             {data.Items.map((product) => {
             return (
                 <div key={product.id} item={product} className="product-card">
-                    <img
-                        src={product.image}
-                        alt={product.name}
-                        width="220"
-                        height="200"
-                        className="item-img"
-                        ></img>
-                        <h5>{product.name}</h5>
-                        <p className="item-price">{product.price} kr</p>
-                    <button className="to-cart-btn">
-                        Add to cart
-                    </button>
+                    <img src={product.image} alt={product.name}
+                    width="220" height="200" className="item-img" />
+                    <h3>{product.name}</h3>
+                    <p className="item-price">{product.price} kr</p>
+                <button className="to-cart-btn">Add to cart</button>
                 </div>
           )
         })}
