6c36f0fa7a6f1a8bf908eff29aff4633af8abd82
Romina Ghaderi <rominaa.ghaderi94@gmail.com>
2023-07-25 18:01:08 +0200

    la till lite ändringar för bakgrund på homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import '../styles.css'
+import img1 from '../productImages/background-image.jpg'
 
 const Home = () => {
   return (
     <main>
-    <div className="HomePage">
-        <h1>Welcome to my E-commerce app</h1>
-    </div>
+
+      <div className='imgbanner-container'>
+        
+        <img src={img1} />
+
+
+            <div className='img-caption text-center mx-auto'>
+
+            <h1>Welcome to my E-commerce app</h1>
+
+
+            </div>
+
+        </div>
+
     </main>
   )
 }
